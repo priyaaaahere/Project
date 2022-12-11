d2edf8d8f95f522c697c7cc8c0182550ef44d423
priyaaaahere <priyadarshinimohapatra1023@gmail.com>
2022-12-11 17:28:17 +0530

    first commit

---
+++ contact_us.js
@@ -0,0 +1,9 @@
+function sub(){
+    var a= confirm("You Query Has Been Submitted")
+    if (a==true){
+        document.write("Thank You For Reaching Out To Us :)")
+    }
+    else{
+        document.write("Cancelled")
+    }
+}
---
+++ payment.js
@@ -0,0 +1,9 @@
+function pay1(){
+    var a= confirm("Do you want to make this payment")
+    if (a==true){
+        document.write("The payment has been processed")
+    }
+    else{
+        document.write("Cancelled")
+    }
+}
